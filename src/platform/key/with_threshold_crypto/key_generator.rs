17bb474dbf3865d2107ef28904bf1dfdc3ce4a7a
tera06 <terazumi.hoshun@gmail.com>
2026-05-08 17:38:14 +0900

    [test] : Added tests in key_generator.rs.

--- src/platform/key/with_threshold_crypto/key_generator.rs
+++ src/platform/key/with_threshold_crypto/key_generator.rs
@@ -27,6 +27,9 @@ impl GenerateKey for KeyGenerator {
         ),
         Self::TError,
     > {
+        if (threshold > num_divide) || (num_divide < 1) || (threshold < 1) {
+            return Err(KeyGeneratorError::InvalidArgument);
+        }
         let mut rng = thread_rng();
         let secret_key_set = SecretKeySet::random(threshold - 1, &mut rng);
         let public_key_set = secret_key_set.public_keys();
@@ -41,6 +44,51 @@ impl GenerateKey for KeyGenerator {
 
 #[derive(Error, Debug)]
 pub enum KeyGeneratorError {
+    #[error("Invalid argument")]
+    InvalidArgument,
     #[error("Failed to generate secret key")]
     FailedGenerateSecretKey,
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn key_generator_generate_keys_success() {
+        let key_generator = KeyGenerator;
+        let result = key_generator.generate_keys(2, 3);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn key_generator_generate_keys_invalid_arg_threshold_is_greater_num_divide() {
+        let key_generator = KeyGenerator;
+        let result = key_generator.generate_keys(3, 2);
+        assert!(matches!(
+            result.err().unwrap(),
+            KeyGeneratorError::InvalidArgument
+        ));
+    }
+
+    #[test]
+    fn key_generator_generates_keys_invalid_arg_threshold_is_zero() {
+        let key_generator = KeyGenerator;
+        let result = key_generator.generate_keys(0, 2);
+        assert!(matches!(
+            result.err().unwrap(),
+            KeyGeneratorError::InvalidArgument
+        ));
+    }
+
+    #[test]
+    fn key_generator_generates_keys_invalid_arg_num_divide_is_zero() {
+        let key_generator = KeyGenerator;
+        let result = key_generator.generate_keys(2, 0);
+        assert!(matches!(
+            result.err().unwrap(),
+            KeyGeneratorError::InvalidArgument
+        ));
+    }
 }
