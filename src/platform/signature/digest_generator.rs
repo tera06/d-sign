9db35ee38a08cc125f7b5b064ee967a7e0d7250e
tera06 <terazumi.hoshun@gmail.com>
2026-06-02 13:33:50 +0900

    [test] : Added tests for digest_generator.rs.

--- src/platform/signature/digest_generator.rs
+++ src/platform/signature/digest_generator.rs
@@ -14,6 +14,9 @@ impl GenerateDigest for DigestGenarator {
         &self,
         message: &str,
     ) -> Result<crate::core::model::signature::Digest<Self::TDigest>, Self::TError> {
+        if message.is_empty() {
+            return Err(DigestGenaratorError::MessageIsEmpty);
+        }
         let mut hasher = Sha256::new();
         hasher.update(&message);
         let digest = hasher.finalize();
@@ -25,4 +28,30 @@ impl GenerateDigest for DigestGenarator {
 }
 
 #[derive(Error, Debug)]
-pub enum DigestGenaratorError {}
+pub enum DigestGenaratorError {
+    #[error("Message is empty")]
+    MessageIsEmpty,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn digest_generator_generate_digest_success() {
+        let digest_generator = DigestGenarator;
+
+        let message = "message";
+        let result = digest_generator.generate_digest(message);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn digest_generator_generate_digest_fail_when_message_is_empty() {
+        let digest_generator = DigestGenarator;
+
+        let message = "";
+        let result = digest_generator.generate_digest(message);
+        assert!(result.is_err());
+    }
+}
